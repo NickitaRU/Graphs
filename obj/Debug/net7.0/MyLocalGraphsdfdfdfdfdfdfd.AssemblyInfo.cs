ac5b5c8388d67e5f37f495ff16111c3fee0d125e
NickitaRU <105883622+NickitaRU@users.noreply.github.com>
2024-08-23 13:44:56 +0300

    123

--- obj/Debug/net7.0/MyLocalGraphsdfdfdfdfdfdfd.AssemblyInfo.cs
+++ obj/Debug/net7.0/MyLocalGraphsdfdfdfdfdfdfd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLocalGraphsdfdfdfdfdfdfd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae9e24d072e4f2b60dea5a4f68e11ccfff70922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfe136597c26a3e8e09448e9579d9c550af3313")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLocalGraphsdfdfdfdfdfdfd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLocalGraphsdfdfdfdfdfdfd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
